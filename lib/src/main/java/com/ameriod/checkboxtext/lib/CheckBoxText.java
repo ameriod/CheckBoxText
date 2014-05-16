4ac0108499ff0110256bd17975baf19b9b98cdf5
parker <a>
2014-05-15 23:28:57 -0400

    added attrs to the lib

--- lib/src/main/java/com/ameriod/checkboxtext/lib/CheckBoxText.java
+++ lib/src/main/java/com/ameriod/checkboxtext/lib/CheckBoxText.java
@@ -1,8 +1,12 @@
 package com.ameriod.checkboxtext.lib;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -12,6 +16,8 @@ import android.widget.Checkable;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Parker on 5/14/2014.
@@ -24,6 +30,13 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
     private CheckBox mCheckBox;
     private TextView mTextView;
 
+    private int mTextOrientation;
+    private float mTextSize;
+    private int mTextStyle;
+    private String mText;
+    private ColorStateList mTextColor;
+    private Drawable mCheckboxBackground;
+
     /**
      * Listener to forward CompoundButton onCheckChangeListener stuff to the checkbox
      */
@@ -31,59 +44,99 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
     /**
      * Orients the text to the right of the checkbox
      */
-    public static final String TEXT_RIGHT = "right";
+    public static final int TEXT_RIGHT = 0;
     /**
      * Orients the text to the left of the checkbox
      */
-    public static final String TEXT_LEFT = "left";
+    public static final int TEXT_LEFT = 1;
     /**
      * Orients the text above of the checkbox
      */
-    public static final String TEXT_ABOVE = "above";
+    public static final int TEXT_ABOVE = 2;
     /**
      * Orients the text below the checkbox
      */
-    public static final String TEXT_BELOW = "below";
+    public static final int TEXT_BELOW = 3;
 
     /**
      * For debugging
      */
     private static final String TAG = "CheckBoxText";
 
-    public CheckBoxText(Context context, String orientation) {
+    /**
+     * Orientations to pick from
+     * <p/>
+     * TEXT_RIGHT = 0;
+     * TEXT_LEFT = 1;
+     * TEXT_ABOVE = 2;
+     * TEXT_BELOW = 3;
+     *
+     * @param orientation
+     * @param context
+     */
+    public CheckBoxText(Context context, int orientation) {
         super(context);
-        build(orientation);
+        mTextOrientation = orientation;
+        build();
     }
 
     public CheckBoxText(Context context) {
         super(context);
-        build(TEXT_RIGHT);
+        build();
     }
 
     public CheckBoxText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        build(TEXT_RIGHT);
+        setAttrs(context, attrs);
+        build();
     }
 
     public CheckBoxText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        build(TEXT_RIGHT);
+        setAttrs(context, attrs);
+        build();
     }
 
-    private void build(String orientation) {
+    private void setAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CheckBoxText, 0, 0);
+
+        try {
+            mTextOrientation = a.getInt(R.styleable.CheckBoxText_orientation, TEXT_RIGHT);
+            mTextSize = a.getDimensionPixelSize(R.styleable.CheckBoxText_textSize, -1);
+            // getColor can not handle references (They can be color state lists)
+            mTextColor = a.getColorStateList(R.styleable.CheckBoxText_textColor);
+            mTextStyle = a.getInt(R.styleable.CheckBoxText_textStyle, -1);
+            mText = a.getString(R.styleable.CheckBoxText_text);
+            mCheckboxBackground = a.getDrawable(R.styleable.CheckBoxText_checkboxBackground);
+        } finally {
+            a.recycle();
+        }
+
+    }
+
+    /**
+     * Orientations to pick from
+     * <p/>
+     * TEXT_RIGHT = 0;
+     * TEXT_LEFT = 1;
+     * TEXT_ABOVE = 2;
+     * TEXT_BELOW = 3;
+     */
+    private void build() {
         LayoutInflater inflater = LayoutInflater.from(getContext());
-        if (TextUtils.equals(orientation, TEXT_BELOW)) {
+        if (mTextOrientation == TEXT_BELOW) {
             inflater.inflate(R.layout.checkbox_text_bottom, this, true);
-        } else if (TextUtils.equals(orientation, TEXT_LEFT)) {
+        } else if (mTextOrientation == TEXT_LEFT) {
             inflater.inflate(R.layout.checkbox_text_left, this, true);
-        } else if (TextUtils.equals(orientation, TEXT_ABOVE)) {
+        } else if (mTextOrientation == TEXT_ABOVE) {
             inflater.inflate(R.layout.checkbox_text_top, this, true);
-        } else if (TextUtils.equals(orientation, TEXT_RIGHT)) {
+        } else if (mTextOrientation == TEXT_RIGHT) {
             inflater.inflate(R.layout.checkbox_text_right, this, true);
         } else {
             // default to right orientation
             inflater.inflate(R.layout.checkbox_text_right, this, true);
         }
+
         mCheckBox = (CheckBox) findViewById(R.id.checkbox_text_box);
         mTextView = (TextView) findViewById(R.id.checkbox_text_text);
         // make everything clickable
@@ -91,11 +144,33 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
         mCheckBox.setOnClickListener(this);
         mTextView.setOnClickListener(this);
 
+        // set the attrs that were passed in
+        if (mTextSize > 0) {
+            setTextSize(mTextSize);
+        }
+
+        if (mTextStyle > 0) {
+            setTypeface(null, mTextStyle);
+        }
+
+        if (mTextColor != null) {
+            setTextColor(mTextColor);
+        }
+
+        if (!TextUtils.isEmpty(mText)) {
+            setText(mText);
+        }
+
+        if (mCheckboxBackground != null) {
+            setBackgroundCheckBox(mCheckboxBackground);
+        }
+
         // set the mCheckBox tag to the mTextView text
         mCheckBox.setTag(mTextView.getText().toString());
         // set the checkbox text to blank
         mCheckBox.setText("");
     }
+
 
     @Override
     public void setChecked(boolean checked) {
@@ -144,6 +219,7 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
      * @param text
      */
     public void setText(String text) {
+        mText = text;
         mTextView.setText(text);
         mCheckBox.setTag(text);
     }
@@ -155,7 +231,9 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
      */
     public void setText(int resId) {
         mTextView.setText(resId);
-        mCheckBox.setTag(getContext().getString(resId));
+        String text = getContext().getString(resId);
+        mText = text;
+        mCheckBox.setTag(text);
     }
 
     public CharSequence getText() {
@@ -170,6 +248,9 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
         mTextView.setTypeface(tf);
     }
 
+    public void setTypeface(Typeface tf, int style) {
+        mTextView.setTypeface(tf, style);
+    }
     /**
      * @param size
      * @link TextView.setTextSize(float size)
@@ -183,11 +264,17 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
         mTextView.setTextColor(color);
     }
 
+    public void setTextColor(ColorStateList textColorStateList) {
+        mTextColor = textColorStateList;
+        mTextView.setTextColor(textColorStateList);
+    }
+
     /**
      * @param size
      * @link TextView.setTextSize(int unit, float size)
      */
     public void setTextSize(int unit, float size) {
+        mTextSize = size;
         mTextView.setTextSize(unit, size);
     }
 
@@ -202,18 +289,12 @@ public class CheckBoxText extends RelativeLayout implements Checkable, View.OnCl
      * @parama background
      */
     public void setBackgroundCheckBox(Drawable background) {
-        mCheckBox.setBackground(background);
-    }
-
-    /**
-     * @link CheckBox.setBackgroundDrawablCheckBox(Drawable background)
-     * <p/>
-     * Use setBackGroundCheckBox instead
-     * @parama background
-     */
-    @Deprecated
-    public void setBackgroundDrawablCheckBox(Drawable background) {
-        setBackgroundDrawable(background);
+        mCheckboxBackground = background;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mCheckBox.setBackground(background);
+        } else {
+            mCheckBox.setBackgroundDrawable(background);
+        }
     }
 
     /**
